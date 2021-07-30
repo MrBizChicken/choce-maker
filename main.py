4ab790da1ccace26ffc69fd48b558001dd97a6d4
MrBizChicken <jordanchubbycheecks@gmail.com>
2021-07-30 10:11:37 -0500

    made anser choicer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-
-
+import threading
 
 
 
@@ -10,18 +9,16 @@ def choice_maker():
     now = datetime.now()
     second = str(now.strftime("%S"))
     print(second)
-    if int(second) == int(second) + 9 * int(second):
-
-
-
-        if int(second) // 2 == 0:
-            file_object = open("log.txt", "a")
-            file_object.write("       even          ")
-            file_object.close()
-        else:
-            file_object = open("log.txt", "a")
-            file_object.write("       odd         ")
-            file_object.close()
+    if int(second) // 2 == 0:
+        threading.Timer(9.0, choice_maker).start()
+        file_object = open("log.txt", "a")
+        file_object.write("       even          ")
+        file_object.close()
+    else:
+        threading.Timer(9.0, choice_maker).start()
+        file_object = open("log.txt", "a")
+        file_object.write("       odd         ")
+        file_object.close()
 
 while True:
     choice_maker()
