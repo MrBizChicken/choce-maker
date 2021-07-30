aee211db2b22da2140dd5ac5de0421ce9fcb1cf5
MrBizChicken <jordanchubbycheecks@gmail.com>
2021-07-30 13:07:29 -0500

    made a log and random even or odd checking

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import threading
+import random
 
 
 
@@ -7,18 +8,32 @@ import threading
 
 def choice_maker():
     now = datetime.now()
+
     second = str(now.strftime("%S"))
+    minute = str(now.strftime("%M"))
+    hour = str(now.strftime("%H"))
     print(second)
-    if int(second) // 2 == 0:
-        threading.Timer(9.0, choice_maker).start()
+
+    if int(minute) % 2 == 0:
+        threading.Timer(random.randint(0, 9), choice_maker).start()
         file_object = open("log.txt", "a")
+        log_object = open("time_of_log.txt", "a")
         file_object.write("       even          ")
+        log_object.write(str(datetime.now()))
         file_object.close()
+        log_object.close()
     else:
-        threading.Timer(9.0, choice_maker).start()
+        threading.Timer(random.randint(0, 9), choice_maker).start()
         file_object = open("log.txt", "a")
         file_object.write("       odd         ")
+        log_object = open("time_of_log.txt", "a")
+        log_object.write(str(datetime.now()))
         file_object.close()
+        log_object.close()
+
+
+
+
 
 while True:
     choice_maker()
